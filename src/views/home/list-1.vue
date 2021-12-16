c599a70152aa63c006ad18a913e7d4fc2ea4858e
jccjd <1340944538@qq.com>
2021-12-16 06:58:37 -1000

    Add files via upload

--- src/views/home/list-1.vue
+++ src/views/home/list-1.vue
@@ -1,5 +1,42 @@
 <template>
-    <div class="login">
-        <h1>list - 1</h1>
-    </div>
+<div class="detail">
+  <div class="markdown-body">
+      <VueMarkdown :source='code' ></VueMarkdown>
+  </div>
+</div>
 </template>
+
+<script>
+import VueMarkdown from 'vue-markdown'
+import hljs from 'highlight.js'
+import 'highlight.js/styles/atom-one-dark.css'
+import 'github-markdown-css/github-markdown.css'
+
+const highlightCode = () => {
+  const preEl = document.querySelectorAll('pre')
+
+  preEl.forEach((el) => {
+    hljs.highlightBlock(el)
+  })
+}
+
+export default {
+    components: {
+        VueMarkdown
+    },
+    data() {
+        return{
+    
+       code:' # hello \n```python \n import hello  \n ``` \n **do it**\n butonfsd', // 要解析的markdown语法的内容
+    }
+  },
+ mounted () {
+        highlightCode()
+    },
+
+    updated () {
+        highlightCode()
+    }
+    
+  }
+</script>
